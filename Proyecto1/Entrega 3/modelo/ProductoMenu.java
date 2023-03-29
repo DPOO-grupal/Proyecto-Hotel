f4f69e40bb7e3700aa5dfe6ea0005c9d88004c58
Juan David Roa Moyano <jd.roa@uniandes.edu.co>
2023-03-29 18:18:33 -0500

    Avance

--- Proyecto1/Entrega 3/modelo/ProductoMenu.java
+++ Proyecto1/Entrega 3/modelo/ProductoMenu.java
@@ -4,9 +4,11 @@ import java.sql.Date;
 
 public class ProductoMenu {
 	
+//Atributos
 	private Date horario;
 	private boolean llevable;
 	
+//Constructor
 	public ProductoMenu(Date horario, boolean llevable) {
 		this.horario = horario;
 		this.llevable = llevable;
--- Proyecto1/Entrega 3/modelo/Restaurante.java
+++ Proyecto1/Entrega 3/modelo/Restaurante.java
@@ -3,9 +3,40 @@ package modelo;
 import java.util.ArrayList;
 
 public class Restaurante {
+	
+//Atributos
 	private ArrayList<ProductoMenu> menu;
 	
-	public Restaurante() {
+//Constructor
+	public Restaurante() 
+	{
 		this.menu = new ArrayList<ProductoMenu>();
 	}
+	
+	public void añadirProducto(ProductoMenu producto) 
+	{
+		menu.add(producto);
+	}
+	
+	public void quitarProducto(ProductoMenu producto) 
+	{
+		for (int i = 0; i < menu.size(); i++) 
+		{
+			if (producto.equals(menu.get(i))) 
+			{
+				menu.remove(i);
+			}
+		}
+	}
+	
+	public ArrayList<ProductoMenu> getMenu()
+	{
+		return this.menu;
+	}
+	
+	public ProductoMenu getProducto(int id) 
+	{
+		return menu.get(id);	
+	}
+	
 }
