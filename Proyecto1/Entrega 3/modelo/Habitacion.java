120ca5a6444bbe4ffaa33df6cce16ea90986e49f
Juan David Roa Moyano <jd.roa@uniandes.edu.co>
2023-03-30 08:40:32 -0500

    Avance habitacion

--- Proyecto1/Entrega 3/modelo/Habitacion.java
+++ Proyecto1/Entrega 3/modelo/Habitacion.java
@@ -1,8 +1,43 @@
 package modelo;
 
+import java.util.*;
+
 public class Habitacion {
 
+//Atributos 
+	private int id;
+	private int capacidad;
+	private ArrayList<Servicio> listaServicios;
+
+//Constructor
+	public Habitacion(String tipo, int capacidad, int id) 
+	{
+		this.capacidad=capacidad;
+		this.id=id;
+	}
+	
+//Metodos
+	public double getPrecio(Date fechaI, Date fechaF) 
+	{
+		return 0;
+	}
+	
+	public int getCapacidad () 
+	{
+		return 0;
+	}	
+	
+	public void añadirServicioHabitacion (Servicio servicio) 
+	{
+		
+	}
+	
 	public TipoHabitacion getTipo() {
 		return TipoHabitacion.ESTANDAR;
+	}
+	
+	public String getCaracteristicas() 
+	{
+		return null;
 	}
 }
