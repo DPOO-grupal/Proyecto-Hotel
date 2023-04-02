b8fbe4e06f900dc78d031d1318745b6c3511130a
Juan David Roa Moyano <jd.roa@uniandes.edu.co>
2023-04-02 14:28:47 -0500

    update

--- Proyecto1/Entrega 3/vista/Aplicacion.java
+++ Proyecto1/Entrega 3/vista/Aplicacion.java
@@ -341,7 +341,7 @@ public class Aplicacion {
 				} else {
 					esLlevable = "(No llevable)";
 				}
-				System.out.println(nombre + ": " + precio +" "+ esLlevable);
+				System.out.println(nombre + ": " + precio + "$" +" "+ esLlevable);
 			}
 		}
 		
