5e1f717e5dde6d873ad5576327786e07b57f34af
Santiago Tinjaca Roncancio <s.tinjaca@uniandes.edu.co>
2023-04-27 19:31:50 -0500

    Esqueleto entrega 2

---
+++ Proyecto 2/src/Vista/TarifasAdminFrame.java
@@ -0,0 +1,19 @@
+package Vista;
+import java.awt.GraphicsConfiguration;
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class TarifasAdminFrame extends JFrame {
+	private JPanel panelVolver;
+	private JPanel panelCrear;
+	private JPanel panelInfo;
+	
+	public TarifasAdminFrame() {
+		
+		
+		
+	}
+
+}
